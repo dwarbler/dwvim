5a3731412b083b7f4393fa1e5ed9488623abdf4b
dwarbler <92753567+dwarbler@users.noreply.github.com>
2025-03-24 21:13:59 -0500

    feat: add ruff (need to fix duplicate messages)

--- init.lua
+++ init.lua
@@ -665,8 +665,7 @@ require('lazy').setup({
       local servers = {
         -- clangd = {},
         -- gopls = {},
-        -- pyright = {},
-        ruff = {},
+        pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
@@ -709,6 +708,7 @@ require('lazy').setup({
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
+        'ruff',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
